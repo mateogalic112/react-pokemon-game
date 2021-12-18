e2a9a28a10a921043e51d41e5649368ec20af453
Mateo Galic <mateo.galic@asynclabs.co>
2021-12-19 00:02:28 +0100

    Update battle logic

--- src/components/BattleField.tsx
+++ src/components/BattleField.tsx
@@ -1,5 +1,6 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 import { useState } from 'react'
+import Battle from '../models/Battle'
 import Pokemon, { Move } from '../models/Pokemon'
 import PokemonCard from './PokemonCard'
 import PokemonOpponentCard from './PokemonOpponentCard'
@@ -10,77 +11,60 @@ interface IBattleFieldProps {
 }
 
 const BattleField = ({ pokemon, opponent }: IBattleFieldProps) => {
+  const battle = new Battle(pokemon, opponent)
+
   const [pokeHealth, setPokeHealth] = useState(pokemon.getHp())
   const [opponentHealth, setOpponentHealth] = useState(opponent.getHp())
+
   const [turn, setTurn] = useState(0)
   const [messages, setMessages] = useState<string[]>([])
 
   const opponentTurn = turn % 2 === 1
 
-  const displayMessages = messages
-
-  const handleAttack = (
-    attacker: Pokemon,
-    defender: Pokemon,
+  const onPokemonAttack = (
+    move: Move,
     health: number,
     setHealth: React.Dispatch<React.SetStateAction<number>>,
-  ) => (move: Move): void => {
-    displayMessages.push(
-      `${attacker.getName()} attacks with ${move.name.toUpperCase()} and ${
-        move.damage
-      } damage`,
+  ) => {
+    const { damage, messages } = battle.attackOpponent(opponentTurn)(
+      move,
+      health,
     )
 
-    if (defender.dodge()) {
-      displayMessages.push(`${defender.getName()} dodged the attack!`)
-    }
+    setMessages((prev) => [...prev, ...messages])
 
-    if (checkForGameEnd(move.damage, health)) {
-      displayMessages.push(`Game over, ${attacker.getName()} wins!`)
-      setMessages([...displayMessages])
-      return
-    }
-
-    setMessages([...displayMessages])
-
-    setHealth((prevHealth) => prevHealth - move.damage)
+    setHealth((prevHealth) => prevHealth - damage)
 
     setTurn(turn + 1)
   }
 
-  const checkForGameEnd = (damage: number, health: number): boolean => {
-    return damage >= health
-  }
-
   return (
-    <Box bg="green.100" p={4}>
-      <PokemonOpponentCard
-        pokemon={opponent}
-        attack={handleAttack(opponent, pokemon, pokeHealth, setPokeHealth)}
-        hp={opponentHealth}
-        active={opponentTurn}
-      />
+    <Flex bg="green.100" p={4}>
+      <Box flexBasis="75%">
+        <PokemonOpponentCard
+          pokemon={opponent}
+          attack={(move) => onPokemonAttack(move, pokeHealth, setPokeHealth)}
+          hp={opponentHealth}
+          active={opponentTurn}
+        />
 
-      <Box height={5} />
+        <Box height={5} />
 
-      {messages.map((message) => (
-        <p>{message}</p>
-      ))}
-
-      <Box height={5} />
-
-      <PokemonCard
-        pokemon={pokemon}
-        attack={handleAttack(
-          pokemon,
-          opponent,
-          opponentHealth,
-          setOpponentHealth,
-        )}
-        hp={pokeHealth}
-        active={!opponentTurn}
-      />
-    </Box>
+        <PokemonCard
+          pokemon={pokemon}
+          attack={(move) =>
+            onPokemonAttack(move, opponentHealth, setOpponentHealth)
+          }
+          hp={pokeHealth}
+          active={!opponentTurn}
+        />
+      </Box>
+      <Flex direction="column" pt={10}>
+        {messages.map((message, index) => (
+          <p key={index}>{message}</p>
+        ))}
+      </Flex>
+    </Flex>
   )
 }
 
--- src/models/Battle.ts
+++ src/models/Battle.ts
@@ -1,42 +1,59 @@
-import Pokemon from './Pokemon'
+import Pokemon, { Move } from './Pokemon'
 
 class Battle {
-  myPokemon: Pokemon
-  myPokemonHp: number
-  enemyPokemon: Pokemon
-  enemyPokemonHp: number
-  turn = 0
+  attacker: Pokemon
+  defender: Pokemon
+  // battle messages
+  messages: string[] = []
 
-  constructor(pokemonOne: Pokemon, pokemonTwo: Pokemon) {
-    this.myPokemon = pokemonOne
-    this.myPokemonHp = pokemonOne.getHp()
-    this.enemyPokemon = pokemonTwo
-    this.enemyPokemonHp = pokemonTwo.getHp()
+  constructor(attaker: Pokemon, defender: Pokemon) {
+    this.attacker = attaker
+    this.defender = defender
   }
 
-  pokeAttack(index: number): { myHp: number; enemyHp: number } {
-    if (this.turn % 2 === 0) {
-      const damage = this.myPokemon.attack(index)
-      this.enemyPokemonHp = this.enemyPokemonHp - damage
-    } else {
-      const damage = this.enemyPokemon.attack(index)
-      this.myPokemonHp = this.myPokemonHp - damage
+  private checkForGameEnd = (damage: number, health: number): boolean => {
+    return damage >= health
+  }
+
+  handleBattleAttack = (attacker: Pokemon, defender: Pokemon) => (
+    move: Move,
+    health: number,
+  ): { damage: number; messages: string[] } => {
+    this.messages.push(
+      `${attacker.getName()} attacks with ${move.name.toUpperCase()} and ${
+        move.damage
+      } damage`,
+    )
+
+    if (defender.dodge()) {
+      this.messages.push(`${defender.getName()} dodged the attack!`)
+
+      return {
+        damage: 0,
+        messages: this.messages,
+      }
     }
 
-    this.turn++
+    if (this.checkForGameEnd(move.damage, health)) {
+      this.messages.push(`Game over, ${attacker.getName()} wins!`)
+
+      return {
+        damage: move.damage,
+        messages: this.messages,
+      }
+    }
 
     return {
-      myHp: this.myPokemonHp,
-      enemyHp: this.enemyPokemonHp,
+      damage: move.damage,
+      messages: this.messages,
     }
   }
 
-  getMyPokemonHp() {
-    return this.myPokemonHp
-  }
-
-  getEnemyPokemonHp() {
-    return this.enemyPokemonHp
+  attackOpponent = (opponentTurn: boolean) => {
+    if (!opponentTurn) {
+      return this.handleBattleAttack(this.attacker, this.defender)
+    }
+    return this.handleBattleAttack(this.defender, this.attacker)
   }
 }
 
