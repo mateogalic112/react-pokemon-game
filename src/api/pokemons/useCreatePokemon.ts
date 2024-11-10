5d3dada8af5662de6524db56cb453949b81aa5b6
matteoo.eth <mateogalic112@gmail.com>
2024-11-10 12:21:18 +0100

    create pokemon

--- src/api/pokemons/useCreatePokemon.ts
+++ src/api/pokemons/useCreatePokemon.ts
@@ -1,35 +1,29 @@
-import { useMutation, useQueryClient } from 'react-query'
-import { useSelector } from 'react-redux'
-import { selectUser } from '../../redux/user'
-import api from '../base'
-import { IPokemonDB } from '../models/PokemonDB'
+import apiConfig from '@/config/api'
+import { useMutation } from '@tanstack/react-query'
 
-interface ICreatePokemonRequest {
+interface CreatePokemonPayload {
+  pokemon_id: number
   hp: number
-  pokemonID: number
-  pokeTrainerId: number
+  trainer_id: number
 }
 
-const createPokemon = async (
-  request: ICreatePokemonRequest
-): Promise<IPokemonDB> => {
-  const response = await api.post<IPokemonDB>('pokemons', request)
-  return response.data
+const createPokemon = async (payload: CreatePokemonPayload) => {
+  const response = await fetch(`${apiConfig.baseURL}/pokemons`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+  return response.json()
 }
 
 export const useCreatePokemon = () => {
-  const queryClient = useQueryClient()
-  const user = useSelector(selectUser)
-  const trainerId = user.authData?.trainerId
-
-  return useMutation(
-    (request: ICreatePokemonRequest) => createPokemon(request),
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries(['trainers', trainerId])
-        queryClient.invalidateQueries(['trainers', 'pokemons', trainerId])
-      },
-      onError: (error: Error) => {},
-    }
-  )
+  return useMutation({
+    mutationFn: (payload: CreatePokemonPayload) => createPokemon(payload),
+    onSuccess: () => {},
+    onError: (error: Error) => {
+      // TODO show toast
+    },
+  })
 }
